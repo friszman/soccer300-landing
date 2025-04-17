a23903d1e481939c3bb065cc683942f5cfaf9bc3
friszman <joe.frisz@enovapay.com>
2025-04-16 21:05:45 -0500

    Update index.js
    Further center rendering

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-white">
-      <div className="w-full max-w-screen-lg px-4 flex justify-center items-center">
+      <div className="flex justify-center items-center w-full px-4">
         <Image
           src="/stadium-bg.jpg"
           alt="Stadium Background"
+          layout="intrinsic"
           width={1316}
           height={768}
-          className="w-full h-auto object-contain"
+          className="mx-auto"
           priority
         />
       </div>
