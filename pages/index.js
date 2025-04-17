600bd6ce7620b7ebff6c20621f55b71a8356851f
friszman <joe.frisz@enovapay.com>
2025-04-16 21:11:16 -0500

    Update index.js
    Centring again

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-white">
-      <div className="flex justify-center items-center w-full px-4">
+    <div className="relative w-screen h-screen bg-white overflow-hidden flex items-center justify-center">
+      <div className="relative w-full h-full">
         <Image
           src="/stadium-bg.jpg"
           alt="Stadium Background"
-          layout="intrinsic"
-          width={1316}
-          height={768}
-          className="mx-auto"
+          fill
+          className="object-contain"
           priority
         />
       </div>
