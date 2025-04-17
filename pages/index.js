dfdaa09200f0c9e7b46d91722cdd7f0961e19f11
friszman <joe.frisz@enovapay.com>
2025-04-16 20:58:02 -0500

    Update index.js
    More centring of image and rendering on phone

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-white">
-      <div className="max-w-full px-4">
+      <div className="w-full max-w-screen-lg px-4 flex justify-center items-center">
         <Image
           src="/stadium-bg.jpg"
           alt="Stadium Background"
           width={1316}
           height={768}
           className="w-full h-auto object-contain"
+          priority
         />
       </div>
     </div>
