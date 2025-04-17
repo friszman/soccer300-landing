c7c3f84edb40079b2c109f41185ae57556002100
friszman <joe.frisz@enovapay.com>
2025-04-16 21:38:33 -0500

    Update index.js
    Fixed image overlay issue by forcing full-screen container

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div
-      className="w-screen h-screen bg-no-repeat bg-center bg-cover flex items-center justify-center"
+      className="fixed inset-0 bg-white bg-no-repeat bg-center bg-cover flex items-center justify-center"
       style={{ backgroundImage: "url('/stadium-bg.jpg')" }}
     >
       <div className="bg-black bg-opacity-60 px-6 py-4 rounded">
