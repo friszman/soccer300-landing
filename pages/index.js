c1f56ceabf820a9225fdfbd7ce8514e7c30a4979
friszman <joe.frisz@enovapay.com>
2025-04-16 21:29:32 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-white">
-      <div className="w-full max-w-screen-xl px-4">
-        <Image
-          src="/stadium-bg.jpg"
-          alt="Stadium Background"
-          width={1920}
-          height={1080}
-          className="w-full h-auto mx-auto"
-          priority
-        />
-      </div>
+    <div
+      className="w-screen h-screen bg-no-repeat bg-center bg-cover"
+      style={{ backgroundImage: "url('/stadium-bg.jpg')" }}
+    >
+      {/* Optional content can go here, centered */}
     </div>
   );
 }
